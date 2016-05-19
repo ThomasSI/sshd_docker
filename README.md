f221e4a8c021daa91e4e4162fa4cc289b82f02ae
Thomas Schulz <schulz@seamlessinteraction.com>
2016-05-19 12:23:15 +0200

    test image generate

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # sshd_docker
+
+
+#Look into the container
+docker run  -it -P --entrypoint="/bin/bash" sshd:1.0
+
+
+
+# Build container
+git clone ..
+
+docker build -t sshd:1.0 .
+docker run  -d -P  sshd:1.0
+
+
+
+
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
+rootpw="$ROOTPW"
 
+echo "$rootpw"
+
+echo echo  ${#rootpw}
+
+file="/rootpass/rootpw"
+
+if [ ${#rootpw} -ge 5 ]
+then
+    echo "Set new root pw"
+    echo "rootpw set to: $rootpw"
+
+    echo "root:$rootpw" | chpasswd
+    echo $newrootpw > "$file"
+else
+    echo "Check for existing rootpw."
+    if [ -f "$file" ]
+    then
+        echo "rootpw is already set execute sshd..."
+    else
+        echo "renerate new rootpw an create file with it..."
+        newrootpw=$(cat /dev/urandom | tr -dc 'a-zA-Z' | fold -w 10 | head -n 1)
+        echo "root:$newrootpw" | chpasswd
+
+        echo "rootpw set to: $newrootpw"
+        echo $newrootpw > "$file"
+    fi
+
+fi
 
 
 exec "/usr/sbin/sshd" "-D"
